afd7bc64262408ec34cf4ac8556918a8ff4ed84a
Jackson Jung <z5397791@ad.unsw.edu.au>
2024-06-25 18:39:04 +1000

    Update dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -41,19 +41,32 @@ export default function Dashboard() {
 
   return (
     <div className='max-w-5xl mx-auto min-h-screen'>
-      <div className='m-6 flex justify-between'>
-        <div className=''>
-          <Link href='/' className='mr-3'>
-            <Button variant='outline'>Home</Button>
-          </Link>
-          <AlertDialogDemo />
+      <div className='flex flex-col justify-center min-h-screen bg-background'>
+        <div className='flex justify-between mt-12'>
+          <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl'>
+            &nbsp;2024 Goal 🎯
+          </h1>
+          <div className='flex items-center gap-3'>
+            <Link href='/'>
+              <Button variant='outline'>Home</Button>
+            </Link>
+            <AlertDialogDemo />
+            <ModeToggle />
+          </div>
         </div>
-        <ModeToggle />
-      </div>
-      <div className='flex flex-col items-center justify-center min-h-screen bg-background'>
+        <br />
+        <br />
+        <h2 className='scroll-m-20 border-b pb-1 text-3xl font-semibold tracking-tight first:mt-0'>
+          &nbsp;12 Week Goals
+        </h2>
         <div className='w-full flex justify-center items-center'>
           <GoalList goals={goals} onEdit={handleEditGoal} onDelete={handleDeleteGoal} />
         </div>
+        <br />
+        <br />
+        <h2 className='scroll-m-20 border-b pb-1 text-3xl font-semibold tracking-tight first:mt-0'>
+          &nbsp;Tasks
+        </h2>
         {[...Array(12)].map((_, index) => (
           <WeeklyTable key={index} weekNumber={index + 1} goals={goals} />
         ))}
